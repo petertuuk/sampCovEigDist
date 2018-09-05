f704c837a80335ad834a0ccc770dd179bc975aa8
Peter Tuuk <petertuuk@gatech.edu>
2018-09-05 13:02:12 -0400

    more updates for v0.7/1.0 and some code cleanup

--- runSampCovEigDist.jl
+++ runSampCovEigDist.jl
@@ -5,34 +5,34 @@ if v >= v"0.7"
 end
 include("sampCovEigDist.jl")
 if v >= v"0.7"
-    ee = range(5,stop=75,length=400)
+    ee = range(5, stop=75, length=400)
 else
-    ee = linspace(5,75,400)
+    ee = linspace(5, 75, 400)
 end
 
-evweight = [2*length(ee) 1;[fill(1.0,length(ee)) ee]]
+evweight = [2*length(ee) 1; [fill(1.0,length(ee)) ee]]
 const c = .5
 
 ## Calc/Plot
 doPlot = true
 if doPlot
-    density1, zvec, cdf = sampCovEigDist(evweight,c)
-    evweight2 = [.8 1;.1 4;.1 10];
-    density2, zvec2, cdf2  = sampCovEigDist(evweight2,c,zvec)
-    density3, zvec3, cdf3  = sampCovEigDist([1 1],c,zvec)
+    density1, zvec, cdf = sampCovEigDist(evweight, c)
+    evweight2 = [.8 1; .1 4; .1 10];
+    density2, zvec2, cdf2  = sampCovEigDist(evweight2, c, zvec)
+    evweight3 = [1 1]
+    density3, zvec3, cdf3  = sampCovEigDist(evweight3, c, zvec)
 
-    using Plots;gr();
-    plot(zvec,density1,xscale=:log10,yscale=:log10)
-    plot!(zvec,density2)
-    plot!(zvec,density3, ylims=(1e-3,10), xlims=(.07,100))
+    using Plots;pyplot();
+    plot( zvec, density1 .+ 1e-16)
+    plot!(zvec, density2 .+ 1e-16)
+    plot!(zvec, density3 .+ 1e-16, ylims=(1e-3,10), xlims=(.07,100), xscale=:log10, yscale=:log10)
     gui()
-    # closeall()
 end
 
-sampCovEigDist(evweight,c)
+sampCovEigDist(evweight, c)
 
 using BenchmarkTools
-@btime sampCovEigDist(evweight,c)
+@btime sampCovEigDist(evweight, c)
 
 ## profile
 v = VERSION
@@ -40,11 +40,13 @@ if v >= v"0.7"
     using Profile
 end
 Profile.init()
-Profile.clear_malloc_data
 Profile.clear()
-@profile sampCovEigDist(evweight,c)
-f = open("profile.prof","w")
+@profile sampCovEigDist(evweight, c)
+f = open("profile.prof", "w")
 Profile.print(f)
 close(f)
 # using ProfileView
 # ProfileView.view()
+
+Profile.clear_malloc_data()
+sampCovEigDist(evweight, c)
--- sampCovEigDist.jl
+++ sampCovEigDist.jl
@@ -23,30 +23,29 @@ function sampCovEigDist(evweight, c, zvec2in=0)
   nMbar = 5000 # sufficient for most
   nZvec = 5000
 
-  evweight[:,1] = evweight[:,1]./sum(evweight[:,1])
-  if v.major>0 || v.minor>6
-    evweight = round.(evweight,digits=6)
+  evweight[:,1] = evweight[:, 1] ./ sum(evweight[:, 1])
+  if v >= v"0.7"
+    evweight = round.(evweight, digits=6)
   else
-    evweight = round.(evweight,6)
+    evweight = round.(evweight, 6)
   end
 
   #generate functions to compute common integrals
   cumputeInteg,cumputeIntegSq = makeIntegralFuncs(evweight)
 
   # find start and end-points where intervals where eigenvalues live
-  intervalStarts,intervalEnds = getIntervals(nMbar,evweight,cumputeInteg)
+  intervalStarts,intervalEnds = getIntervals(nMbar, evweight, cumputeInteg)
 
   zvec2::Vector{Float64} = []
-  if length(zvec2in)>1
+  if length(zvec2in) > 1
     zvec2 = zvec2in;
     nZvec = length(zvec2)
   else
     ll = 3;
-
     if v >= v"0.7"
-      zvec2 = range(0,stop=(1.01*intervalEnds[end]).^(1/ll),length=nZvec+1).^ll
+      zvec2 = range(0, stop=(1.01*intervalEnds[end]).^(1/ll), length=nZvec+1).^ll
     else
-      zvec2 = linspace(0,(1.01*intervalEnds[end]).^(1/ll),nZvec+1).^ll
+      zvec2 = linspace(0, (1.01*intervalEnds[end]).^(1/ll), nZvec+1).^ll
     end
     zvec2 = zvec2[2:end];
   end
@@ -57,19 +56,19 @@ function sampCovEigDist(evweight, c, zvec2in=0)
   guess = 0.0im
   fminres = 0.0im
   fminresOld = 0.0im
-  cfact = 1/min(c,1)/pi
+  cfact = 1/min(c, 1) / pi
 
   # Calculate which points in eigenvalue sample space are in the support
-  islt = fill(0.0,size(zvec2))
-  ielt = fill(0.0,size(zvec2))
+  islt = fill(0.0, size(zvec2))
+  ielt = fill(0.0, size(zvec2))
   intervalStarts99::Vector{Float64} = intervalStarts*.99
   intervalEnds101::Vector{Float64} = intervalEnds*1.01
   for iz in eachindex(zvec2)
     for ii1 in eachindex(intervalStarts99)
-      if zvec2[iz]>intervalStarts99[ii1]
+      if zvec2[iz] > intervalStarts99[ii1]
         islt[iz] += 1
       end
-      if zvec2[iz]>intervalEnds101[ii1]
+      if zvec2[iz] > intervalEnds101[ii1]
         ielt[iz] += 1
       end
     end
@@ -104,7 +103,7 @@ function sampCovEigDist(evweight, c, zvec2in=0)
       # Check the result to ensure consistency
       z_recalc = -1/fminres + cumputeInteg(fminres);
       z_resid = abs(z_recalc - zast);
-      if z_resid > 1e-3 && z_resid/abs(zast) > 1e-3
+      if ( z_resid > 1e-3 ) && ( z_resid/abs(zast) > 1e-3 )
         wentBadly = true;
         print("some divergence in z: $(z_resid/abs(zast))");
       else
@@ -114,11 +113,11 @@ function sampCovEigDist(evweight, c, zvec2in=0)
   end
 
   # Calculate CDF using trapezoidal integration
-  cdf = trapz(zvec2,density)
-  if cdf[end]>1.02 || cdf[end]<1/1.02
-    warn("cdf was $(cdf[end])!\n")
+  cdf = trapz(zvec2, density)
+  if cdf[end] > 1.02 || cdf[end] < 1/1.02
+    @warn "cdf was $(cdf[end])!\n"
   end
-  cdf = cdf/cdf[end]
+  cdf = cdf / cdf[end]
 
   return density, zvec2, cdf
 end
@@ -137,7 +136,7 @@ end
   return x
 end
 
-function trapz(x,y)
+function trapz(x, y)
 # Trapezoid integration
   area = fill(0.0,size(x))
   lx = length(x)
@@ -147,34 +146,34 @@ function trapz(x,y)
   return area
 end
 
-function makeMbarVec(evweight,nMbar)
-  startm = min(log10(1/maximum(evweight[:,2])/10),-3)
-  endm = max(-log10(1/minimum(evweight[:,2])),3)
-  if v.major>0 || v.minor>6
-    mbarvecHalf = 10 .^ range(startm,stop=endm,length=Int(nMbar/2))
+function makeMbarVec(evweight, nMbar)
+  startm = min( log10( 1/maximum(evweight[:,2])/10 ), -3)
+  endm =   max(-log10( 1/minimum(evweight[:,2])    ),  3)
+  if v >= v"0.7"
+    mbarvecHalf = 10 .^ range(startm, stop=endm, length=Int(nMbar/2))
   else
-    mbarvecHalf = logspace(startm,endm,Int(nMbar/2))
+    mbarvecHalf = logspace(startm, endm, Int(nMbar/2))
   end
-  mbarvec = sort([-mbarvecHalf;mbarvecHalf])
+  mbarvec = sort([-mbarvecHalf; mbarvecHalf])
 end
 
 function makeIntegralFuncs(evweight)
 # Generate the functions that will compute commonly used integral and
 # its partial derivative
   ut::Array{Float64,1} = unique(evweight[:,2]);
-  ht::Array{Float64,1} = fill(0.0,size(ut))
+  ht::Array{Float64,1} = fill(0.0, size(ut))
   for iu in eachindex(ut)
-    ixEv = evweight[:,2].== ut[iu]
-    ht[iu] = sum(evweight[ixEv,1])
+    ixEv = evweight[:,2] .== ut[iu]
+    ht[iu] = sum(evweight[ixEv, 1])
   end
 
-  uh::Array{Float64,1} = ut.*ht;
+  uh::Array{Float64,1} = ut .* ht;
   uhSq::Array{Float64,1} = (ut.^2) .* ht;
   # uhSq::Array{Float64,1} = ut .* ut .* ht;
   @inline function cumputeInteg(mbar)
     funcsum::Complex{Float64} = 0
     @inbounds for ii2 in eachindex(ut)
-      funcsum += uh[ii2]/(1+ut[ii2]*mbar);
+      funcsum += uh[ii2] / (1 + ut[ii2] * mbar);
     end
     funcsum *= c
     return funcsum
@@ -182,39 +181,41 @@ function makeIntegralFuncs(evweight)
   @inline function cumputeIntegSq(mbar)
     funcsum::Complex{Float64} = 0
     @inbounds for ii3 in eachindex(ut)
-      funcsum += uhSq[ii3]/(1+ut[ii3]*mbar)^2;
+      funcsum += uhSq[ii3] / (1 + ut[ii3] * mbar)^2;
     end
     funcsum *= c
     return funcsum
   end
 
-  return cumputeInteg,cumputeIntegSq
+  return cumputeInteg, cumputeIntegSq
 end
 
-function getIntervals(nMbar,evweight,cumputeInteg)
+function getIntervals(nMbar, evweight, cumputeInteg)
 # find start and stop values for each interval in which
 # the spectral distribution will have non-zero mass
-  mbarvec = makeMbarVec(evweight,nMbar)
+  mbarvec = makeMbarVec(evweight, nMbar)
 
   zvec = fill(0.0,size(mbarvec))
-  @simd for i1 in eachindex(mbarvec)
-    mbar = mbarvec[i1];
-    zvec[i1] = -1/mbar + cumputeInteg(mbar);
+  cvec = cumputeInteg.(mbarvec)
+  for i1 in eachindex(mbarvec)
+    zvec[i1] = -1/mbarvec[i1] + cvec[i1]
   end
 
-  eigInds::Array{Int32,1} = fill(0.0,size(evweight,1))
+  eigInds::Array{Int32,1} = fill(0.0, size(evweight, 1))
   evDiff = 0.0
-  for uu in 1:size(evweight,1)
-    evInv::Float64 = -1/evweight[uu,2]
-    minval = 9999.9
-    minind = 0
-    for iu = eachindex(mbarvec)
-      evDiff = abs(evInv-mbarvec[iu])
-      if  evDiff < minval
-        minval = abs(evInv-mbarvec[iu])
-        minind = iu
-      end
-    end
+  for uu in 1:size(evweight, 1)
+    # minval = 9999.9
+    # minind = 0
+    # dvec = abs.(-1/evweight[uu,2] .- mbarvec)
+    # for iu = eachindex(dvec)
+    #   if  dvec[iu] < minval
+    #     minval = dvec[iu]
+    #     minind = iu
+    #   end
+    # end
+
+    minind = argmin( abs.(-1 / evweight[uu,2] .- mbarvec) )
+
     eigInds[uu] = minind
   end
   isPeak = unique(eigInds);
@@ -227,29 +228,29 @@ function getIntervals(nMbar,evweight,cumputeInteg)
 
   if v >= v"0.7"
     findPicks = findall(picks)
-    dfpUp = findall(diff([-Inf;findPicks]).>1)
-    dfpDown = findall(diff([findPicks;Inf]).>1)
+    dfpUp = findall(diff([-Inf; findPicks]) .> 1)
+    dfpDown = findall(diff([findPicks; Inf]) .> 1)
   else
     findPicks = find(picks)
-    dfpUp = find(diff([-Inf;findPicks]).>1)
-    dfpDown = find(diff([findPicks;Inf]).>1)
+    dfpUp = find(diff([-Inf; findPicks]) .> 1)
+    dfpDown = find(diff([findPicks; Inf]) .> 1)
   end
 
   zvs = zvec[findPicks[dfpUp]]
   zve = zvec[findPicks[dfpDown]]
   if v >= v"0.7"
-    complementOfSupport = sort([zvs zve],dims=1)
+    complementOfSupport = sort([zvs zve], dims=1)
   else
-    complementOfSupport = sort([zvs zve],1)
+    complementOfSupport = sort([zvs zve], 1)
   end
-  intervalStarts = complementOfSupport[1:end-1,2];
-  intervalEnds = complementOfSupport[2:end,1];
-  if length(intervalStarts)>0
-    intLength = intervalEnds-intervalStarts
-    intBreak = intervalStarts[2:end]-intervalEnds[1:end-1]
-    if length(intBreak)>0
-      intervalStarts = intervalStarts[[true;intBreak.>0]]
-      intervalEnds = intervalEnds[[intBreak.>0;true]]
+  intervalStarts = complementOfSupport[1:end-1, 2];
+  intervalEnds = complementOfSupport[2:end, 1];
+  if length(intervalStarts) > 0
+    intLength = intervalEnds - intervalStarts
+    intBreak = intervalStarts[2:end] - intervalEnds[1:end-1]
+    if length(intBreak) > 0
+      intervalStarts = intervalStarts[[true; intBreak.>0]]
+      intervalEnds = intervalEnds[[intBreak.>0; true]]
     end
   end
 
@@ -257,11 +258,11 @@ function getIntervals(nMbar,evweight,cumputeInteg)
 
   if any( intervalStarts[2:end] .<= intervalEnds[1:end-1] )
     dse = intervalStarts[2:end] - intervalEnds[1:end-1]
-    takeStarts = [1;find(dse.>0)+1];
-    takeEnds = [find(dse.>0);length(intervalStarts)];
+    takeStarts = [1; find(dse .> 0) + 1];
+    takeEnds = [find(dse .> 0); length(intervalStarts)];
 
     intervalStarts = intervalStarts[takeStarts];
     intervalEnds = intervalEnds[takeEnds];
   end
-  return intervalStarts,intervalEnds
+  return intervalStarts, intervalEnds
 end
